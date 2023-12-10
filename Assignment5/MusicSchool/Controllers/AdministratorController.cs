18c6a4aa0cdae5e94feb75382807b60654cf20ea
Trey1717 <mathtrey17@gmail.com>
2023-12-10 16:36:02 -0600

    Admin Page and Coloring

---
+++ Assignment5/MusicSchool/Controllers/AdministratorController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MusicSchool.Data;
+using MusicSchool.Models;
+
+namespace Music_Shop.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        private readonly MusicShopContext _context;
+
+        public AdministratorController(MusicShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Administrator
+        public async Task<IActionResult> Index()
+        {
+              return _context.Music != null ? 
+                          View(await _context.Music.ToListAsync()) :
+                          Problem("Entity set 'MusicShopContext.Music'  is null.");
+        }
+
+        // GET: Administrator/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Music == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.Music
+                .FirstOrDefaultAsync(m => m.MusicId == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            return View(music);
+        }
+
+        // GET: Administrator/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administrator/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MusicId,SongTitle,Genre,Perfomer,Price")] Music music)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(music);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(music);
+        }
+
+        // GET: Administrator/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Music == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.Music.FindAsync(id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+            return View(music);
+        }
+
+        // POST: Administrator/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MusicId,SongTitle,Genre,Perfomer,Price")] Music music)
+        {
+            if (id != music.MusicId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(music);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MusicExists(music.MusicId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(music);
+        }
+
+        // GET: Administrator/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Music == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.Music
+                .FirstOrDefaultAsync(m => m.MusicId == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            return View(music);
+        }
+
+        // POST: Administrator/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Music == null)
+            {
+                return Problem("Entity set 'MusicShopContext.Music'  is null.");
+            }
+            var music = await _context.Music.FindAsync(id);
+            if (music != null)
+            {
+                _context.Music.Remove(music);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MusicExists(int id)
+        {
+          return (_context.Music?.Any(e => e.MusicId == id)).GetValueOrDefault();
+        }
+    }
+}
